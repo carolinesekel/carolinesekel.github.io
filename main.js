3c77f1c9ba5ae62ca1c3fb7b6e51cd3cf782aea3
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-12 15:51:08 -0400

    starting work with js

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(
+    function(){
+        $('.menu-toggler').on('click', function(){
+            $(this).toggleClass('open');
+            $('.nav-top').toggleClass('open');
+        })
+    }
+);
