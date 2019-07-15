3bb8b53a0b3c30ba596d056b3a6ef703135d58f4
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-15 15:48:34 -0400

    placeholder images and troubleshooting

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ $(document).ready(
             $(this).toggleClass('open');
             $('.nav-top').toggleClass('open');
         });
+        $('.nav-top .nav-link').on('click', function() {
+            $('.menu-toggler').removeClass('open');
+            $('.top-nav').removeClass('open');
+        });
     }
+
 );
