dfab6fd4bc075adb06e8ac6bdd92e0b5a8b65251
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-17 16:27:50 -0400

    more

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ $(document).ready(
             $('.menu-toggler').removeClass('open');
             $('.nav-top').removeClass('open');
         });
+        $('nav a[href*="#"]').on('click', function() {
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top - 100
+            }, 2000);
+        });
     }
 
 );
