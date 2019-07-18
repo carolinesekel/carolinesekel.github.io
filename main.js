8bfcfb96fe46c0b69a988bb45e45edbcf0c86bcd
Caroline Sekel <csekel@mm-mac-10689.mediamath.local>
2019-07-18 15:21:19 -0400

    smooth scrolling

--- main.js
+++ main.js
@@ -8,9 +8,14 @@ $(document).ready(
             $('.menu-toggler').removeClass('open');
             $('.nav-top').removeClass('open');
         });
-        $('nav-top a[href*="#"]').on('click', function() {
+        $('a').on('click', function() {
             $('html, body').animate({
                 scrollTop: $($(this).attr('href')).offset().top - 100
+            }, 2000);
+        });
+        $('#up').on('click', function() {
+            $('html, body').animate({
+                scrollTop:0
             }, 2000);
         });
     }
